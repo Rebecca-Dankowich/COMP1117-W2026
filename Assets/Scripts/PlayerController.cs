6c353c80fc27252001335adcf6177fee0df0d207
Rebecca Dankowich <r.dankowich@gmail.com>
2026-01-07 13:42:24 -0500

    added player movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerStats stats;
+
+    //Components
+    private Rigidbody2D rBody;
+
+    //Field Variables
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        //initialize
+        rBody = GetComponent<Rigidbody2D>();
+        
+        stats = new PlayerStats();
+        int something = stats.MoveSpeed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    void ApplyMovement()
+    {
+        float velocityX = moveInput.x;
+        rBody.linearVelocity = new Vector2(velocityX * stats.MoveSpeed, rBody.linearVelocity.y);
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerStats
+{
+    private int moveSpeed = 5;
+    
+    public int MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+    }
+}
