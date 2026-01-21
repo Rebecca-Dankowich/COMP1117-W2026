f5bf61fb1c992cc92b631c4eb9e299bd301c1e2e
Rebecca Dankowich <r.dankowich@gmail.com>
2026-01-21 12:40:08 -0500

    Week 3 Main Quest

--- Assets/Scripts/PlayerLandingEffect.cs
+++ Assets/Scripts/PlayerLandingEffect.cs
@@ -4,11 +4,24 @@ public class PlayerLandingEffect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem landingParticles;
 
+    private bool wasGrounded = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !wasGrounded)
+        {
+            landingParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            landingParticles.Play();
+            
+            wasGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            landingParticles.Play();
+            wasGrounded = false;
         }
     }
 }
